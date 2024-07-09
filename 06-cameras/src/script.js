39939963ca00d6874563f5bddc2034f1fa56ceee
Muhammad Ali <muhammadwarraich007@gmail.com>
2024-07-09 15:26:11 +0500

    🔥 NEW: full screen, pixel ratio and resizing

--- 06-cameras/src/script.js
+++ 06-cameras/src/script.js
@@ -1,33 +1,35 @@
 import * as THREE from 'three'
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 
-/**
- * Base
- */
-// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-
-
 // cursor
-
 const cursor = {
     x: 0,
     y: 0
 }
 
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
 window.addEventListener('mousemove', (e) => {
     cursor.x = e.clientX / sizes.width - 0.5;
     cursor.y = e.clientY / sizes.height - 0.5;
 })
-
-
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600
-}
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+window.addEventListener('dblclick', () => {
+    if(!document.fullscreenElement) canvas.requestFullscreen();
+    else document.exitFullscreen();
+})
 
 // Scene
 const scene = new THREE.Scene()
@@ -35,7 +37,7 @@ const scene = new THREE.Scene()
 // Object
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+    new THREE.MeshBasicMaterial({ color: "white" })
 )
 scene.add(mesh)
 
@@ -55,7 +57,8 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.setSize(sizes.width, sizes.height)
+renderer.setSize(sizes.width, sizes.height);
+
 
 // Animate
 const clock = new THREE.Clock()
